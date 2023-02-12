3fd3d4de21708105c35c74637751819b128a709f
Frampt <mikael.eriksson@iths.se>
2023-02-12 12:09:19 +0100

    First commit.

--- iceCreamNet7API/Program.cs
+++ iceCreamNet7API/Program.cs
@@ -103,6 +103,7 @@ public class IceCreamData
     public string Id { get; set; }
     public string Name { get; set; }
     public string Info { get; set; }
+    public string Img { get; set; }
     public string Type { get; set; }
     public string Company { get; set; }
     public nutritionalContent nutritionalContent { get; set; }
