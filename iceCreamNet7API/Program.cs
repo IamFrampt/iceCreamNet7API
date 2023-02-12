e70ffa03f5dfaf38753ea9be41530201a396af6e
Frampt <mikael.eriksson@iths.se>
2023-02-12 12:10:08 +0100

    added img source

--- iceCreamNet7API/Program.cs
+++ iceCreamNet7API/Program.cs
@@ -113,6 +113,7 @@ public class AddIceCreamData
 {
     public string Name { get; set; }
     public string Info { get; set; }
+    public string Img { get; set; }
     public string Type { get; set; }
     public string Company { get; set; }
     public nutritionalContent nutritionalContent { get; set; }
