d2ea8f746b15939bfff0c3dce81941f44beb22a4
Frampt <mikael.eriksson@iths.se>
2023-02-16 17:44:58 +0100

    fiale

--- iceCreamNet7API/Program.cs
+++ iceCreamNet7API/Program.cs
@@ -26,8 +26,7 @@ var app = builder.Build();
 app.UseCors(builder => builder
  .AllowAnyOrigin()
  .AllowAnyMethod()
- .AllowAnyHeader()
- .AllowCredentials());
+ .AllowAnyHeader());
 
 
 // Configure the HTTP request pipeline.
